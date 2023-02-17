fbd3b9a2b65593c7e5b91160951a85c11a5245bc
Victor Ebi <ebivikky@gmail.com>
2023-02-16 18:11:49 -0800

    printf in peace

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - the script that prints Hello world
- * Return: 0
+ * Return: 0 |
  */ |
 int main(void); |
 {	printf(""with proper grammer, but its outcome is a piece of art, " \n")
