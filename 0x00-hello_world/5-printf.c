c9e1c8848cced8e73ae6c4730c9866916f82038f
Victor Ebi <ebivikky@gmail.com>
2023-02-16 17:15:56 -0800

    script to printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - the scripttht prints Hello world
+ * Return: 0
+ */
+int main(void);
+{
+	printf("with proper grammer, but its outcome is a piece of art,\n")
+	return (0);
+}
