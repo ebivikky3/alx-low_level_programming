67794c09d83a12708cbd9feb9162f9d18e9c58a9
Victor Ebi <ebivikky@gmail.com>
2023-02-16 17:29:27 -0800

    code or printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - the scripttht prints Hello world
+ * main - the script that prints Hello world
  * Return: 0
  */
 int main(void);
 {
-	printf("with proper grammer, but its outcome is a piece of art,\n")
+	printf("with proper grammer, but its outcome is a piece of art, \n")
 	return (0);
 }
