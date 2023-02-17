cf8b0c19c83388aa71cf197034d9e5a5d740d90d
Victor Ebi <ebivikky@gmail.com>
2023-02-17 02:49:38 -0800

    how printf works

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
-| #include <stdio.h> |
+#include <stdio.h>
 /**
- * main - the script that prints Hello world
- * Return: 0
- */ |
-int main(void); |
-{	printf(""with proper grammer, but its outcome is a piece of art, " \n")
-	| return (0);
+ * main - A programe that shows the function of printf
+ * Return (0)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art," \n")
+	return 0;
 }
