4c6b94f2611eed9f16a5999e992e6faff4ba8f76
Victor Ebi <ebivikky@gmail.com>
2023-02-20 20:17:01 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i < '9'; i++)
+	putchar("%d", i);
+	putchar("\n");
+	return (0);
+}
