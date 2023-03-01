d1948fd783669abbbdef190dc573d34634ff6c3a
Victor Ebi <ebivikky@gmail.com>
2023-02-28 20:45:26 -0800

    swapping of integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * Swap_nt - is a script that swaps the value of two integers.
+ * Swap_int - is a script that swaps the value of two integers.
  * *a is the integer to be  swapped.
  * *b is the  integer to swap to.
  */
 void swap_int(int *a, int *b)
-/* The script that swaps the value of two integers*/
+/* The function that swaps the value of two integers*/
 {
 	int temp = *a;
 	*a = *b;
