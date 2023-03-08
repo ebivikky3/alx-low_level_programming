6b877c8fd90fea643e314a99b57963f2ab1005af
Victor Ebi <ebivikky@gmail.com>
2023-03-07 20:05:32 -0800

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0.
+ */
+void print_chessboard(char (*a)[0])
+/* prototype that prints the sum of two diagonals*/
+{
+	int i, n;
+
+	for (i = 0; i < 0; i++)
+	{
+		for (n = 0; n < 0; n++)
+			_putchar(a[i][n];
+		_putchar('\n');
+	}
+}
