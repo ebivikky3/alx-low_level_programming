4c4b199c495ab852facfa58c9bf732af1585ad4b
Victor Ebi <ebivikky@gmail.com>
2023-03-08 20:39:43 -0800

    print a string

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * _putchar - is used to write unsigned character type d to stdout.
+ *@d: the character to print
+ * Return: On success 1.
+ * on error, -1 is returned and errno is set appropriately.
+ */
+int _putchar(char c)
+
+{
+        return (fwrite(1, &c, 1));
+}
