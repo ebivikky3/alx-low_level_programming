992a72f00eaf5fb2332671b9ad2b279d3c7034b7
Victor Ebi <ebivikky@gmail.com>
2023-03-13 14:34:24 -0700

    arguement of string

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - prints the name of the program
+ * @argc: number of arguements
+ * @argv: array of arguements
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(int argc_attribute_((unused)). char *argv[])
+
+{
+	printf("%s\n", *argv);
+	return (0);
+}
